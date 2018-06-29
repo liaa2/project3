a69d0a99393c032fe6e233deb4c77eb6ec9824f0
Linna Liu <cello1412@gmail.com>
2018-06-29 22:33:38 +1000

    ball straight bouncing & hand movement worked

---
+++ js/main-function.js
@@ -0,0 +1,149 @@
+ var app = app || {};
+
+
+app.createPlane = () => {
+  var planeMaterial = new THREE.MeshLambertMaterial({ color: 0x3080C9 });
+  // let planeWidth = 150;
+  let planeLength = 300;
+  var planeGeometry = new THREE.PlaneGeometry(app.planeWidth, planeLength, 30)
+  // create the surface
+  var plane = new THREE.Mesh(planeGeometry, planeMaterial);
+  plane.rotation.x = -0.5 * Math.PI;
+
+  return plane;
+};
+
+
+app.createBall = () => {
+  var geometry = new THREE.SphereGeometry( 2, 30, 30)
+  var material = new THREE.MeshStandardMaterial( { color: 0xffd046 })
+  var sphere = new THREE.Mesh( geometry, material )
+  sphere.castShadow = true;
+  sphere.position.set(0, 30, 1); //-140);
+  sphere.velocity = new THREE.Vector3(0, 0, 1);
+
+  return sphere;
+};
+
+app.createAmbientlight = () => {
+  var ambientLight = new THREE.AmbientLight (0xffffff, 0.5);
+  return ambientLight;
+}
+
+app.createPointlight = () => {
+  var pointLight = new THREE.PointLight(0xffffff, 1);
+  pointLight.position.set(25,50,25);
+  return pointLight;
+}
+
+app.createHelper = () => {
+  var helper = new THREE.AxesHelper( 10 );
+  return helper
+}
+
+
+
+
+//animation
+app.animate = () => {
+  // console.log("animate");
+
+  // app.sphere.rotation.x += Math.PI/50;
+  // app.sphere.rotation.y += Math.PI/50;
+  //
+  //ball bouncing
+  app.step += app.guiControls.bouncingSpeed;
+  // console.log(app.step);
+  // app.ball.position.x = 20 + (Math.cos(app.step)*10);
+  // app.ball.position.y = 6 + (Math.abs(Math.sin(app.step))*10);
+
+  app.updateBall();
+  // app.ball.position.y = 6 + app.step
+
+  // paddle.rotation.x = Math.PI/2;
+
+  app.stats.update();
+
+  app.renderer.render( app.scene, app.camera );
+  requestAnimationFrame(app.animate);
+}
+
+app.addStats = () => {
+  const stats = new Stats();
+  stats.setMode(0);
+  stats.domElement.style.position = 'absolute';
+  stats.domElement.style.left = '0px';
+  stats.domElement.style.top = '0px';
+
+  document.body.appendChild(stats.domElement);
+
+  return stats;
+};
+
+app.resetBall  = () =>  {
+  app.ball.position.set(0, 30, 1);
+  app.ball.velocity.set(0, 0, 1);
+};
+
+app.updateBall = () => {
+  const pos = app.ball.position;
+  const paddle = app.paddle.position;
+
+  app.ball.position.addScaledVector(
+    app.ball.velocity,
+    app.guiControls.ballVelocityScale
+  );
+  // const dist = app.ball.position.distanceTo( app.paddle.position );
+  // app.guiControls.rollDebug = dist;
+  // //
+  // if ( dist < 1 ) {
+  //   // bounce off bat
+  //   app.ball.velocity.multiplyScalar(-1);
+  // } else if ( app.ball.position.z <= app.wallZ ) {
+  //   // bounce off opposite 'wall'
+  //   app.ball.velocity.multiplyScalar(-1);
+  // } else if(  app.ball.position.z > 200 ){
+  //   app.resetBall();
+  // }
+
+  if( pos.x >= paddle.x - app.paddleWidth/2 &&
+      pos.x <= paddle.x + app.paddleWidth/2){
+
+    if( pos.y >= paddle.y - app.paddleWidth/2 &&
+        pos.y <= paddle.y + app.paddleWidth/2 &&
+        paddle.z - pos.z < 4){
+
+        // if (pos.z - paddle.z < 4){
+          // console.log("Hit!");
+          app.ball.velocity.multiplyScalar(-1);
+        // }
+
+    }
+  } // ball within x range
+
+  if(pos.z <= app.wallZ){
+    app.resetBall();
+  }
+
+  // collision detection:
+  //   determines if any of the rays from the cube's origin to each vertex
+  //		intersects any face of a mesh in the array of target meshes
+  //   for increased collision accuracy, add more vertices to the cube;
+  //		for example, new THREE.CubeGeometry( 64, 64, 64, 8, 8, 8, wireMaterial )
+  //   HOWEVER: when the origin of the ray is within the target mesh, collisions do not occur
+  // var originPoint = app.ball.position.clone();
+  //
+  // for (var vertexIndex = 0; vertexIndex < app.ball.geometry.vertices.length; vertexIndex++)
+  // {
+  //   var localVertex = app.ball.geometry.vertices[vertexIndex].clone();
+  //   var globalVertex = localVertex.applyMatrix4( app.ball.matrix );
+  //   var directionVector = globalVertex.sub( app.ball.position );
+  //
+  //   var ray = new THREE.Raycaster( originPoint, directionVector.clone().normalize() );
+  //   var collisionResults = ray.intersectObject( app.paddle );
+  //   // console.log(collisionResults);
+  //   if ( collisionResults.length > 0 && collisionResults[0].distance < directionVector.length() )
+  //     console.warn(" Hit ");
+  // }
+
+};
--- js/main.js
+++ js/main.js
@@ -1,93 +1,77 @@
 
-let camera;
-let hand;
-let paddle;
-let scene;
-let renderer;
+var app = app || {};
 
-$(document).ready(function(){
+app.planeWidth = 200;
+app.paddleWidth = 12 * app.planeWidth/100;
+app.wallZ = 0; // - 150
+app.fingers = [];
+app.step = 0;
+app.guiControls = {
+  bouncingSpeed: 0.05,
+  rollDebug: '',
+  ballVelocityScale: 1.0
+}
 
+
+
+app.init = () => {
   console.log("loaded");
 
-  var app = {};
-  app.fingers = [];
-
-
-  var step = 0;
-  var controls = {
-    bouncingSpeed: 0.05
-  }
-
-  var gui = new dat.GUI();
-  gui.add(controls, "bouncingSpeed", 0, 1);
-
-
+  app.gui = new dat.GUI();
+  app.gui.add(app.guiControls, "bouncingSpeed", 0, 1);
+  app.gui.add(app.guiControls, "ballVelocityScale", -2, 2);
+  app.gui.add(app.guiControls, "rollDebug").listen();
 
   //set up 3D
-  scene = new THREE.Scene()
+  app.scene = new THREE.Scene()
 
-  camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 )
-  camera.position.set( 0, 80, 200 );
+  app.camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 )
+  app.camera.position.set( 0, 80, 200 );
   // camera.up = new THREE.Vector3(0,1,0);
   // camera.lookAt( scene.position );
 
-  renderer = new THREE.WebGLRenderer({ antialias: true})
+  app.renderer = new THREE.WebGLRenderer({ antialias: true})
 
   //rotate camera
-  var controls = new THREE.OrbitControls( camera );
+  app.controls = new THREE.OrbitControls( app.camera, app.renderer.domElement );
 
 
+  app.renderer.setSize( window.innerWidth, window.innerHeight )
+  document.body.appendChild( app.renderer.domElement )
 
-  renderer.setSize( window.innerWidth, window.innerHeight )
-  document.body.appendChild( renderer.domElement )
-  // camera.position.z = 5
 
-  //resize canvas on resize window automatically
-  window.addEventListener('resize', ()=> {
-    let width = window.innerWidth
-    let height = window.innerHeight
-    renderer.setSize(width, height)
-    camera.aspect = width/height
-    camera.updateProjectionMatrix()
-  })
-
+  //Stats
+  app.stats = app.addStats();
 
   //table - just plane
-  var planeMaterial = new THREE.MeshLambertMaterial({ color: 0x3080C9 });
-  let planeWidth = 200;
-  let planeLength = 300;
-  var planeGeometry = new THREE.PlaneGeometry(planeWidth, planeLength, 30)
-  // create the surface
-  var plane = new THREE.Mesh(planeGeometry, planeMaterial);
-  plane.rotation.x = -0.5 * Math.PI;
-
-  scene.add(plane);
+  app.plane = app.createPlane();
+  app.scene.add(app.plane);
 
   //ball
-  var geometry = new THREE.SphereGeometry( 0.15, 30, 30)
-  var material = new THREE.MeshStandardMaterial( { color: 0xffd046 })
+  app.ball = app.createBall();
+  app.scene.add(app.ball);
 
-  var sphere = new THREE.Mesh ( geometry, material )
-  sphere.castShadow = true;
-  scene.add( sphere )
-  renderer.render( scene, camera )
+  // Light - ambientLight
+  app.ambientLight = app.createAmbientlight();
+  app.scene.add(app.ambientLight);
 
-  //light
-  var ambientLight = new THREE.AmbientLight (0xffffff, 0.5)
-  scene.add(ambientLight)
+  //Light - pointLight
+  app.pointLight = app.createPointlight();
+  app.scene.add(app.pointLight);
 
-  var pointLight = new THREE.PointLight(0xffffff, 1);
-  pointLight.position.set(25,50,25);
-  scene.add(pointLight)
+  //helper - (0,0,0) coordinates
+  app.helper = app.createHelper();
+  app.scene.add(app.helper);
 
   // var helper = new THREE.AxisHelper( 10 );
-  var helper = new THREE.AxesHelper( 10 );
-  scene.add( helper );
+  // var helper = new THREE.AxesHelper( 10 );
+  // scene.add( helper );
 
   //A loader for loading objects in JSON format. This uses the FileLoader internally for loading files.
   //instantiate a loader
-  var loader = new THREE.JSONLoader();
-  loader.load(
+  app.loader = new THREE.JSONLoader();
+  // python -m http.server
+  app.loader.load(
     //resource URl
     '../paddle.js',
     loadPaddle
@@ -96,54 +80,33 @@ $(document).ready(function(){
   //Paddle
   // var x,y,z;
   function loadPaddle(geometry, materials){
-  var scale = planeWidth/100
-   paddle = new THREE.Mesh( geometry, new THREE.MeshFaceMaterial(materials) );
-   paddle.scale.set(scale, scale, scale);
-   paddle.position.set(0,30,120);
-   // paddle.position.set(x,y,z);
-   // paddle.rotation.y = 0.5 * Math.PI;
-   // paddle.rotation.x = -0.5 * Math.PI;
-   paddle.rotation.y = 0;
-   // paddle.rotation.z = -0.5*Math.PI;
-   scene.add(paddle);
-   // renderer.render( scene, camera )
+    console.log('HERE');
+    var scale = app.planeWidth/100;
+    app.paddle = new THREE.Mesh( geometry, new THREE.MeshFaceMaterial(materials) );
+    app.paddle.scale.set(scale, scale, scale);
+    app.paddle.position.set(0,30,120);
+    // paddle.position.set(x,y,z);
+    // paddle.rotation.y = 0.5 * Math.PI;
+    // paddle.rotation.x = -0.5 * Math.PI;
+    app.paddle.rotation.y = 0;
+    // paddle.rotation.z = -0.5*Math.PI;
+    app.scene.add( app.paddle );
+
+    // don't start animating until the paddle is loaded
+    app.animate();
+    // renderer.render( scene, camera )
   }
-
-
-  function render(){
-    //ball's self rotation
-    sphere.rotation.x += Math.PI/50;
-    sphere.rotation.y += Math.PI/50;
-
-    //ball bouncing
-    step += controls.bouncingSpeed;
-    sphere.position.x = 20 + (Math.cos(step)*10);
-    sphere.position.y = 6 + (Math.abs(Math.sin(step))*10);
-
-    // paddle.rotation.x = Math.PI/2;
-
-    //add scene to the screen
-    renderer.render(scene, camera);
-  }
-
-  //animation
-  function animate(){
-    requestAnimationFrame(animate);
-    controls.update()
-    render();
-  }
-  animate();
+  // app.animate();
 
 
   //Leap Motion Controller
   //initiate animationFrame & gestures
-  var options = {
+  app.options = {
     frameEventName: 'animationFrame',
     enableGestures: true,
-
   };
 
-  var controller = Leap.loop(options, function(frame){
+  app.controller = Leap.loop(app.options, function(frame){
     // console.log(frame);
     // console.log(frame.hands);
 
@@ -153,8 +116,33 @@ $(document).ready(function(){
       hand = frame.hands[0]
       handMesh = hand.data('riggedHand.mesh');
       // handMesh.position.set(0, 30, 150 )
-      handMesh.scenePosition(hand.indexFinger.tipPosition, paddle.position);
-      paddle.position.z += 150;
+      handMesh.scenePosition(hand.palmPosition, app.paddle.position);
+      app.paddle.position.z += 150;
+
+      //link hand roll with paddle rotation along y axis
+      // if (frame.hands[0].roll()>=0) {
+      //   app.paddle.rotation.y  += Math.abs((frame.hands[0].roll()))
+      // } else {
+      //   app.paddle.rotation.y -= Math.abs((frame.hands[0].roll()))
+      // }
+
+      // using roll():
+
+      let angle = THREE.Math.mapLinear(
+        frame.hands[0].roll(),   //value to map
+        -2, 2,   //min & max input range
+        Math.PI/4, -Math.PI/4  //min & max output
+      );
+      // app.guiControls.rollDebug = frame.hands[0].pitch() ;
+      // let angle = THREE.Math.mapLinear(
+      //   frame.hands[0].pitch(),   //value to map
+      //   -1, 1,   //min & max input range
+      //   Math.PI/4, -Math.PI/4  //min & max output
+      // );
+
+      angle = THREE.Math.clamp(angle, -Math.PI/4, Math.PI/4);
+      // app.guiControls.rollDebug = angle;
+      app.paddle.rotation.y = angle
     }
 
 
@@ -178,7 +166,7 @@ $(document).ready(function(){
     // }//end if
   })//controller
 
-  controller.use('transform', { scale: planeWidth/1000 })
+  app.controller.use('transform', { scale: app.planeWidth/1000 })
   .use("riggedHand", {
     // boneLabels: function(boneMesh, leapHand){
     //   return boneMesh.name
@@ -196,212 +184,22 @@ $(document).ready(function(){
 
   })
 
-  controller.connect();
+  app.controller.connect();
 
-  // //another cube with wireframe
-  // var geometry = new THREE.BoxGeometry(3,3,3);
-  // var material = new THREE.MeshBasicMaterial({
-  //   color: "#dadada", wireframe: true, transparent: true
-  // })
-  // var wireframeCube = new THREE.Mesh (geometry, material)
-  // scene.add(wireframeCube)
-  //
-  //
-  // function animate(){
-  //   requestAnimationFrame( animate );
-  //   // sphere.rotation.x += 0.04;
-  //   // sphere.rotation.y += 0.04;
-  //   sphere.rotation.x += Math.PI/50;
-  //   sphere.rotation.y += Math.PI/50;
-  //   wireframeCube.rotation.x -= Math.PI/100;
-  //   wireframeCube.rotation.y -= Math.PI/100;
-  //   renderer.render(scene, camera)
-  // }
+  // start off the animation loop
 
-  // animate();
+};
 
-  // var canvas = document.getElementById("canvas");
-  // var ctx = canvas.getContext("2d");
-  //
-  // var options = {
-  //   frameEventName: 'animationFrame',
-  //   enableGestures: true
-  // };
-  //
-  // Leap.loop(options, function(frame){
-  //   ctx.clearRect(0,0, ctx.canvas.width, ctx.canvas.height);
-  //   frame.gestures.forEach(function(gesture){
-  //     if (gesture.type != "swipe") {
-  //       return;
-  //     }
-  //
-  //     var startPosition = frame.interactionBox.normalizePoint(gesture.startPosition);
-  //     var endPosition = frame.interactionBox.normalizePoint(gesture.position);
-  //
-  //     var startX = ctx.canvas.width*startPosition[0];
-  //     // var startY = ctx.canvas.height*startPosition[0];
-  //     var startY = ctx.canvas.width*(1-startPosition[0]);
-  //
-  //     var endX = ctx.canvas.width*endPosition[0];
-  //     var endY = ctx.canvas.width*(1-endPosition[1]);
-  //
-  //
-  //     //Begin a path
-  //     ctx.beginPath();
-  //     //move to position (startX, startY);
-  //     ctx.moveTo(startX, startY);
-  //     //Create a line to position (endX, endY);
-  //     ctx.lineTo(endX, endY);
-  //     //The stroke() method actually draws the path you have defined with all those moveTo() and lineTo() methods. The default color is black.
-  //     ctx.stroke();
-  //   })
-
-  // })
-
-  // Leap.loop({frameEventName: 'animationFrame'}, function(frame){
-  //   ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-  //   frame.pointables.forEach(function(pointable){
-  //     var position = pointable.stabilizedTipPosition;
-  //     var normalized = frame.interactionBox.normalizePoint(position);
-  //     var x = normalized[0]*ctx.canvas.width;
-  //     var y = (1-normalized[1])*ctx.canvas.height;
-  //     ctx.beginPath();
-  //     ctx.rect(x, y, 20, 20);
-  //     ctx.fill();
-  //   })
-  // });//2D canvas drawing loop
-});//end of jquery
-
-//
-// Leap.loop(function(frame){
-//
-//   if(frame.pointables.length > 0){
-//      //Get a pointable and normalize the tip position
-//      // var pointable = frame.pointables[1];
-//      app.pinch = frame.hands[0].pinchStrength;
-//
-//      for (var i = 0; i < frame.pointables.length; i++) {
-//        var position = frame.pointables[i].tipPosition
-//        var interactionBox = frame.interactionBox;
-//        var normalizedPosition = interactionBox.normalizePoint(position, true);
-//        app.fingers[i] = {
-//          x: normalizedPosition[0],
-//          y: normalizedPosition[1],
-//          z: normalizedPosition[2],
-//          extended: frame.hands[0].fingers[i].extended
-//        };
-//      }
-//   }
-//   // console.log(app.fingers);
-// });
+window.onload = app.init;
 
 
+//resize canvas on resize window automatically
+app.onResize = () => {
+  app.width = window.innerWidth;
+  app.height = window.innerHeight;
+  app.camera.aspect = app.width/app.height
+  app.camera.updateProjectionMatrix()
+  app.renderer.setSize(app.width, app.height)
+};
 
-
-// var particles = [];
-//
-// app.controls = {
-//   velocityScale: 0.5,
-//   gravity: 0.0,
-//   lifeDecrement: 0.0
-// };
-//
-// window.onload = function(){
-//   app.gui = new dat.GUI();
-//   app.gui.add(app.controls, 'velocityScale', -1, 1);
-//   app.gui.add(app.controls, 'gravity', -1, 1);
-//   app.gui.add(app.controls, 'lifeDecrement', 0, 0.1);
-// }
-
-// var setup = function(){
-//
-//   createCanvas(windowWidth, windowHeight);// same as window.innerWidth. etc
-//   background(0);
-//   colorMode(HSB, 255);
-//   // fill(255,0,0);//set the fill colour for shapes
-//   // stroke(0, 255, 255)//border colour
-//   noStroke()//no border
-// }
-
-// var drawFingers = function(){
-//
-//   const finger = app.fingers[1];
-//   // console.log(finger, app.pinch);
-//
-//   if( app.pinch >0.65 ){
-//     const size = app.pinch * 20;
-//      fill(finger.z * 255, 255, 255);
-//      ellipse(
-//        finger.x * windowWidth,
-//        (1.0 - finger.y) * windowHeight,
-//        size, size
-//      );
-//   }
-// }
-//
-// var draw = function(){
-//
-//   if(keyIsDown(CONTROL)){
-//     background(0) //clear the patten after it shows
-//   }
-//   const x = mouseX;
-//   const y = mouseY;
-//
-//   const vx = mouseX - pwinMouseX
-//   const vy = mouseY - pwinMouseY -4;
-//   // console.log({vx, vy});
-//
-//   const mouseVel = Math.sqrt(vx*vx + vy*vy); //pythagoras
-//
-//
-//   drawFingers();
-//
-//
-//   if(mouseIsPressed || keyIsDown(SHIFT)){
-//     const size = mouseVel;
-//     const hue = frameCount % 255;
-//     particles.push({x, y, vx, vy, size, hue, mouseVel, life: 1.0});
-//   }
-//   updateParticles();
-// }
-//
-// var updateParticles = function(){
-//   const outputParticles = [];
-//
-//   for (var i = 0; i < particles.length; i++) {
-//     const p = particles[i];
-//
-//     p.x += p.vx*app.controls.velocityScale;//upate position by adding velocity
-//     p.y += p.vy*app.controls.velocityScale;
-//
-//     // p.life -=0.01; //opacity
-//     p.life -=app.controls.lifeDecrement; //opacity
-//
-//     if (p.life > 0) {
-//       outputParticles.push(p)
-//     }
-//
-//     if(p.x >= windowWidth || p.x <= 0){
-//       p.vx *= -0.7 //flip the velocity, i.e. change the sign from pos ->neg or neg -> pos
-//     }
-//
-//     if(p.y >= windowHeight || p.y <=0){
-//       p.vy *=-0.7  //flip the velocity, i.e. change the sign from pos ->neg or neg -> pos
-//     }
-//     p.vy += app.controls.gravity;
-//
-//     fill(p.hue, 255, 255, p.life*255)
-//     ellipse(p.x, p.y, p.size, p.size);
-//   }
-//
-//   particles = outputParticles;
-// }
-//
-// var keyPressed = function(event){
-//   console.log(event);
-//   switch (event.keyCode) {
-//     case 32:
-//       background(0);
-//       break;
-//   }
-// }
+window.addEventListener('resize', app.onResize, false);
